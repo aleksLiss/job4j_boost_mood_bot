ebc516543adf83a629fa305248ec9d4b3a966fa2
aleksei <darklisik.darklisik@yandex.ru>
2025-01-03 18:00:33 +0300

    DI, IoC, Beans, BeanFactory, ApplicationContext [#505289]

--- src/main/java/ru/job4j/telegramm/api/BotCommandHandler.java
+++ src/main/java/ru/job4j/telegramm/api/BotCommandHandler.java
@@ -1,4 +1,12 @@
 package ru.job4j.telegramm.api;
 
+import org.springframework.stereotype.Service;
+import ru.job4j.content.Content;
+
+@Service
 public class BotCommandHandler {
+
+    void receive(Content content) {
+        System.out.println(content);
+    }
 }
