86c202ff88adae40796b6e23696519037857528c
aleksei <darklisik.darklisik@yandex.ru>
2025-01-03 18:44:11 +0300

    Жизненный цикл Spring Bean [#505292 #527171]

--- src/main/java/ru/job4j/business/MoodService.java
+++ src/main/java/ru/job4j/business/MoodService.java
@@ -1,4 +1,19 @@
 package ru.job4j.business;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MoodService {
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Bean is going through @PostConstruct init.");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Bean will be destroyed via @PreDestroy.");
+    }
 }
--- src/main/java/ru/job4j/recommedations/RecommendationEngine.java
+++ src/main/java/ru/job4j/recommedations/RecommendationEngine.java
@@ -1,4 +1,20 @@
 package ru.job4j.recommedations;
 
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+@Component
 public class RecommendationEngine {
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Bean is going through @PostConstruct init.");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Bean will be destroyed via @PreDestroy.");
+    }
 }
