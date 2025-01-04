15f2c572b32d6938ad035e44876179e67a1ac3c4
aleksei <darklisik.darklisik@yandex.ru>
2025-01-04 22:01:49 +0300

    Внедрение коллекций | Поставщики контекста [#505300 #527316]

--- src/main/java/ru/job4j/recommedations/RecommendationEngine.java
+++ src/main/java/ru/job4j/recommedations/RecommendationEngine.java
@@ -1,12 +1,26 @@
 package ru.job4j.recommedations;
 
-
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import ru.job4j.content.Content;
+import ru.job4j.content.ContentProvider;
+import java.util.List;
+import java.util.Random;
 
-@Component
+@Service
 public class RecommendationEngine {
+    private final List<ContentProvider> contents;
+    private static final Random RND = new Random(System.currentTimeMillis());
+
+    public RecommendationEngine(List<ContentProvider> contents) {
+        this.contents = contents;
+    }
+
+    public Content recommendFor(Long chatId, Long moodId) {
+        var index = RND.nextInt(0, contents.size());
+        return contents.get(index).byMood(chatId, moodId);
+    }
 
     @PostConstruct
     public void init() {
