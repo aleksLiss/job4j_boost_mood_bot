7b9d9de6e8637745a31a2d834a48cb4efe355589
aleksei <darklisik.darklisik@yandex.ru>
2025-01-08 22:57:37 +0300

    @Bean | Инициализация базы данных [#505304]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -3,11 +3,21 @@ package ru.job4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import ru.job4j.telegramm.api.TelegramBotService;
+import ru.job4j.model.Award;
+import ru.job4j.model.Mood;
+import ru.job4j.model.MoodContent;
+import ru.job4j.repository.AwardRepository;
+import ru.job4j.repository.MoodContentRepository;
+import ru.job4j.repository.MoodRepository;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@EnableAspectJAutoProxy
 @EnableScheduling
 @SpringBootApplication
 public class Main {
@@ -16,9 +26,71 @@ public class Main {
     }
 
     @Bean
-    public CommandLineRunner initTelegramApi(ApplicationContext context) {
+    CommandLineRunner loadDatabase(MoodRepository moodRepository,
+                                   MoodContentRepository moodContentRepository,
+                                   AwardRepository awardRepository) {
         return args -> {
-            var bot = context.getBean(TelegramBotService.class);
+
+            List<Mood> moods = moodRepository.findAll();
+
+            if (!moods.isEmpty()) {
+                return;
+            }
+            List<MoodContent> data = new ArrayList<>();
+            data.add(new MoodContent(new Mood("Счастливейший на свете \uD83D\uDE0E", true),
+                    "Невероятно! Вы сияете от счастья, продолжайте радоваться жизни."));
+            data.add(new MoodContent(new Mood("Воодушевленное настроение \uD83C\uDF1F", true),
+                    "Потрясающе! Вы в состоянии внутреннего мира и гармонии."));
+            data.add(new MoodContent(new Mood("В состоянии комфорта \u263A\uFE0F", true),
+                    "Отлично! Вы чувствуете себя уютно и спокойно."));
+            data.add(new MoodContent(new Mood("Легкое волнение \uD83C\uDF88", true),
+                    "Замечательно! Немного волнения добавляет жизни краски."));
+            data.add(new MoodContent(new Mood("Сосредоточенное настроение \uD83C\uDFAF", true),
+                    "Хорошо! Ваш фокус на высоте, используйте это время эффективно."));
+            data.add(new MoodContent(new Mood("Тревожное настроение \uD83D\uDE1F", false),
+                    "Не волнуйтесь, всё пройдет. Попробуйте расслабиться и найти источник вашего беспокойства."));
+            data.add(new MoodContent(new Mood("Разочарованное настроение \uD83D\uDE1E", false),
+                    "Бывает. Не позволяйте разочарованию сбить вас с толку, всё наладится."));
+            data.add(new MoodContent(new Mood("Усталое настроение \uD83D\uDE34", false),
+                    "Похоже, вам нужен отдых. Позаботьтесь о себе и отдохните."));
+            data.add(new MoodContent(new Mood("Вдохновенное настроение \uD83D\uDCA1", true),
+                    "Потрясающе! Вы полны идей и энергии для их реализации."));
+            data.add(new MoodContent(new Mood("Раздраженное настроение \uD83D\uDE20", false),
+                    "Попробуйте успокоиться и найти причину раздражения, чтобы исправить ситуацию."));
+            moodRepository.saveAll(data.stream().map(MoodContent::getMood).toList());
+            moodContentRepository.saveAll(data);
+            awardRepository.saveAll(Arrays.asList(
+                    new Award("Смайлик дня", "За 1 день хорошего настроения. " +
+                            "Награда: Веселый смайлик или стикер, отправленный пользователю в качестве поощрения.", 1),
+                    new Award("Настроение недели", "За 7 последовательных дней хорошего или отличного настроения. " +
+                            "Награда: Специальный значок или иконка, отображаемая в профиле пользователя в течение недели.", 7),
+                    new Award("Бонусные очки", "За каждые 3 дня хорошего настроения. " +
+                            "Награда: Очки, которые можно обменять на виртуальные предметы или функции внутри приложения.", 3),
+                    new Award("Персонализированные рекомендации", "После 5 дней хорошего настроения. " +
+                            "Награда: Подборка контента или активности на основе интересов пользователя.", 5),
+                    new Award("Солнечный луч", "За 10 дней непрерывного хорошего настроения. " +
+                            "Награда: Разблокировка новой темы оформления или фона в приложении.", 10),
+                    new Award("Виртуальный подарок", "После 15 дней хорошего настроения. " +
+                            "Награда: Возможность отправить или получить виртуальный подарок внутри приложения.", 15),
+                    new Award("Титул 'Лучезарный'", "За 20 дней хорошего или отличного настроения. " +
+                            "Награда: Специальный титул, отображаемый рядом с именем пользователя.", 20),
+                    new Award("Доступ к премиум-функциям", "После 30 дней хорошего настроения. " +
+                            "Награда: Временный доступ к премиум-функциям или эксклюзивному контенту.", 30),
+                    new Award("Участие в розыгрыше призов", "За каждую неделю хорошего настроения. " +
+                            "Награда: Шанс выиграть призы в ежемесячных розыгрышах.", 7),
+                    new Award("Эксклюзивный контент", "После 25 дней хорошего настроения. " +
+                            "Награда: Доступ к эксклюзивным статьям, видео или мероприятиям.", 25),
+                    new Award("Настроение месяца", "За поддержание хорошего или отличного настроения в течение целого месяца. " +
+                            "Награда: Специальный значок, признание в сообществе или дополнительные привилегии.", 30),
+                    new Award("Физический подарок", "После 60 дней хорошего настроения. " +
+                            "Награда: Возможность получить небольшой физический подарок, например, открытку или фирменный сувенир.", 60),
+                    new Award("Коучинговая сессия", "После 45 дней хорошего настроения. " +
+                            "Награда: Бесплатная сессия с коучем или консультантом для дальнейшего улучшения благополучия.", 45),
+                    new Award("Разблокировка мини-игр", "После 14 дней хорошего настроения. " +
+                            "Награда: Доступ к развлекательным мини-играм внутри приложения.", 14),
+                    new Award("Персональное поздравление", "\"За значимые достижения (например, 50 дней хорошего настроения). " +
+                            "Награда: Персонализированное сообщение от команды приложения или вдохновляющая цитата.\"", 50)
+            ));
         };
     }
 }
--- src/main/java/ru/job4j/repository/MoodContentRepository.java
+++ src/main/java/ru/job4j/repository/MoodContentRepository.java
@@ -3,5 +3,11 @@ package ru.job4j.repository;
 import org.springframework.data.repository.CrudRepository;
 import ru.job4j.model.MoodContent;
 
+import java.util.List;
+
 public interface MoodContentRepository extends CrudRepository<MoodContent, Long> {
+
+    void saveAll(List<MoodContent> moodContentList);
+
+    List<MoodContent> findAll();
 }
