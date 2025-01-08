e9f3c3e3e94845f99115f3480be52a1831a5e351
aleksei <darklisik.darklisik@yandex.ru>
2025-01-08 23:34:03 +0300

    @Bean | Инициализация базы данных [#505304 #527730]

--- src/main/java/ru/job4j/repository/MoodRepository.java
+++ src/main/java/ru/job4j/repository/MoodRepository.java
@@ -3,5 +3,10 @@ package ru.job4j.repository;
 import org.springframework.data.repository.CrudRepository;
 import ru.job4j.model.Mood;
 
+import java.util.List;
+
 public interface MoodRepository extends CrudRepository<Mood, Long> {
+    List<Mood> findAll();
+
+    void saveAll(List<Mood> moods);
 }
